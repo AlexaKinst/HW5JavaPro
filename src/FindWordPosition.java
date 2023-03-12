1f83a3bb9c9721133148a04423e28dd959e97ccd
alexa <alexandra.pilina97@gmail.com>
2023-03-12 21:04:45 +0200

    Initial commit

---
+++ src/FindWordPosition.java
@@ -0,0 +1,13 @@
+public class FindWordPosition {
+    public static void main(String[] args) {
+        String source;
+        String target;
+        boolean b;
+
+        source = "Apple";
+        target = "Java";
+        b = source.contains(target);
+        System.out.println("Перша строка: " + source + ". " + "Друга строка: " + target + ". " + "Співпадіння: " + b);
+    }
+}
+
